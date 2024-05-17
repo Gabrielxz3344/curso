e8becb3ecebfdd1c82a9c68fd933b81103fcf781
Luiz Gabriel Costa De Sousa <0000115632869xsp@aluno.educacao.sp.gov.br>
2024-05-17 17:47:36 -0300

    firts commit

---
+++ exercicio 1.py
@@ -0,0 +1,11 @@
+#solicitar ao usuário que digite um número
+número = float(input("digite um número : "))
+#verificar se o numero é positivo, negativo ou zero
+if número > 0:
+    print ("o número é positivo.")
+elif número < 0:
+    print("o número é negativo.")
+else:
+    print("o número é zero.")
+
+
---
+++ exercício 2.py
@@ -0,0 +1,10 @@
+valor_compra = float(input("digite o valor da compra:"))
+if valor_compra >=100:
+    desconto = valor_compra = 0.1
+    valor_final = valor_compra = desconto
+    print("parábens! voce ganhou um desconto de 10%')")
+    print("valor final de compra: r$ ", valor_final)
+else:
+    print("infezlimente, voce nao ganhou desconto")
+    print("valor final da compra: r$,valor_compra")
+
